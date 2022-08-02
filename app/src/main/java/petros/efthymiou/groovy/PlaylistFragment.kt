b9eb0fb5ef0ac8e1deaa20a5cd695dee992c0328
Pedro Cutino <pedro.cutino23@gmail.com>
2022-08-02 18:29:13 -0500

    .

---
+++ app/src/main/java/petros/efthymiou/groovy/PlaylistFragment.kt
@@ -0,0 +1,37 @@
+package petros.efthymiou.groovy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.LifecycleOwner
+
+
+class PlaylistFragment : Fragment() {
+
+    lateinit var viewModel: PlaylistViewModel
+    lateinit var viewModelFactory: PlaylistViewModelFactory
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+        viewModel.playlists.observe(this as LifecycleOwner) { playlists ->
+            with(view as RecyclerView) {
+                layoutManager = LinearLayoutManager(context)
+                adapter = MyPlaylistRecyclerViewAdapter(playlists)
+            }
+        }
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            PlaylistFragment().apply {}
+    }
+}
